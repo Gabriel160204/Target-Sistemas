e30f2adbb3d33c67d04a70adc7020be23323bdeb
Gabriel Eduardo De Souza <89217994+Gabriel160204@users.noreply.github.com>
2023-03-08 13:33:15 -0300

    Soma = 91

---
+++ 1.java
@@ -0,0 +1,15 @@
+public class App {
+
+	public static void main(String[] args) {
+		
+		int indice = 13,
+			soma = 0,
+			k = 0;
+
+		while (k < indice) {
+			k += 1;
+			soma += k;
+		}
+		System.out.println(soma);
+	}
+}
